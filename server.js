364ee05bacbe0dba866edb9dbb0ef426494e4b8c
Andrew <legero_cam@yahoo.com>
2016-07-17 11:23:53 -0500

    create createserver function, it check for mime type, check to see if a file || directory is been process, and redirect base on conditions

--- server.js
+++ server.js
@@ -11,15 +11,42 @@ var mimeTypes = {
   "css"   : "text/css"
 };
 
-const hostname = '127.0.0.1';
-const port = 3000;
+//Create server
+http.createServer((req, res) => {
+  //parse request containing file name
+  var uri = url.parse(req.url).pathname;
+  //get file name
+  var fileName = path.join(process.cwd(), unescape(uri));
+  console.log('loading ' + uri);
+  var stats;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+  //check to see if requested file is found
+  try {
+    stats = fs.lstatSync(fileName);
+  } catch(ex) {
+    res.writeHead(404, {'Content-type': 'text/plain'});
+    res.write('404 your file is on a vacation \n');
+    res.end();
+    return;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+  }
+  //check to see a file || a directory was return
+  if(stats.isFile()) {
+    var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+    res.writeHead(200, {'Content-type': mimeType});
+
+    var fileStream = fs.createReadStream(fileName);
+    fileStream.pipe(res);
+
+  } else if(stats.isDirectory()) {
+    res.writeHead(302, {
+      'Location' : 'index.html'
+    });
+    res.end();
+  } else{
+    res.writeHead(500, {'Content-Type' : 'text/plain'});
+    res.write('500 Internal Error \n');
+    res.end();
+  }
+
+}).listen(3000);
